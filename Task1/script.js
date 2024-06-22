98b5b9d2ebc55f04269b5cbeab9d51f9050358a9
Kavin Selvam <129585692+S-KAVIN@users.noreply.github.com>
2024-06-22 21:16:10 +0530

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,19 @@
+document.getElementById('home').addEventListener('click', function() {
+    document.body.style.backgroundColor = "yellow";  
+});
+
+document.getElementById('features').addEventListener('click', function() {
+    document.body.style.backgroundColor = "green";  
+});
+
+document.getElementById('pricing').addEventListener('click', function() {
+    document.body.style.backgroundColor = "blue";  
+});
+
+document.getElementById('about').addEventListener('click', function() {
+    document.body.style.backgroundColor = "orange";  
+});
+
+document.getElementById('contact').addEventListener('click', function() {
+    document.body.style.backgroundColor = "pink";  
+});
